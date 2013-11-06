1246041cf826d0883e6e455aa41c9794ae7e5192
khurramfarooq <khurram.farooq26@gmail.com>
2013-11-07 01:50:11 +0500

    second-commit

---
+++ src/com/android/grammarpower/activity/Grammar_Power_MainActivity.java
@@ -0,0 +1,24 @@
+package com.android.grammarpower.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+import com.actionbarsherlock.app.*;
+
+public class Grammar_Power_MainActivity extends SherlockFragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_grammar__power__main);
+	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.grammar__power__main, menu);
+//		return true;
+//	}
+
+}
